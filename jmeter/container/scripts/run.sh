b464391ea8c0a25f8760ae743f257a2856f0e51d
Frederic Giloux <fgiloux@users.noreply.github.com>
2018-07-27 10:38:36 +0200

    Feed JMeter with -J parameters in run script

--- jmeter/container/scripts/run.sh
+++ jmeter/container/scripts/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Parse JMeter parameters, replaces the J_ in J_PARAM with -J -> -JPARAM
+for param in $(printenv | grep J_); do
+    JMETER_PARAMS="$JMETER_PARAMS -J${param:2}"
+done
+
 # Runs tests from JMX file, creates results file and reports dashboard
 for TEST_FILE in $JMETER_BASE/tests/*.jmx; do
     RESULT_FILE=$JMETER_BASE/results/$(basename $TEST_FILE .jmx)_$(date +"%F_%T").jtl
-    $JMETER_HOME/bin/jmeter -n -t $TEST_FILE -l $RESULT_FILE
+    $JMETER_HOME/bin/jmeter -n -t $TEST_FILE -l $RESULT_FILE $JMETER_PARAMS
     # Print results file
     cat $RESULT_FILE
 done
