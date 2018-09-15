4f0f47a5c9570575cd48ebcfc316424767004681
Frederic Giloux <fgiloux@users.noreply.github.com>
2018-09-15 11:14:34 +0200

    Improved pipeline for perf report

--- jmeter/container/scripts/run.sh
+++ jmeter/container/scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dir=${TEST_DIR:-$(date +"%F_%T")}
+dir=${RESULT_SUB_DIR:-$(date +"%F_%T")}
 # Parse JMeter parameters, replaces the J_ in J_PARAM with -J -> -JPARAM
 for param in $(printenv | grep J_); do
     JMETER_PARAMS="$JMETER_PARAMS -J${param:2}"
@@ -8,7 +8,7 @@ done
 
 # Runs tests from JMX file, creates results file and reports dashboard
 for TEST_FILE in $JMETER_BASE/tests/*.jmx; do
-    RESULT_FILE=/opt/jmeter/results/${dir}/(basename $TEST_FILE .jmx).jtl
+    RESULT_FILE=/opt/jmeter/results/${dir}/$(basename $TEST_FILE .jmx).jtl
     $JMETER_HOME/bin/jmeter -n -t $TEST_FILE -l $RESULT_FILE $JMETER_PARAMS
     # Print results file
     cat $RESULT_FILE
